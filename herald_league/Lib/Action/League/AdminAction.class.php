2848d9cf425418facc75964df0304674843150f8
Tairy <tairyguo@gmail.com>
2013-07-05 13:59:39 +0800

    添加后台管理模版

---
+++ herald_league/Lib/Action/League/AdminAction.class.php
@@ -0,0 +1,12 @@
+<?php
+class AdminAction extends Action{
+	public function changeinfo(){
+		$this -> display();
+	}
+	public function addalbum(){
+		$this -> display();
+	}
+	public function addactivity(){
+		$this -> display();
+	}
+}
